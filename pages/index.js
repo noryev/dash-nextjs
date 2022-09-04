4edad4a921ee815349eda6eec1afb894f05e4b45
Logan Lentz <logan.lentz1@gmail.com>
2022-09-03 22:20:05 -0500

    idk what im doing

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,12 @@ import styles from '../styles/Home.module.css';
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
         <title>Create Next App</title>
         <meta name="description" content="Created by Noyev" />
         <link rel="icon" href="/pro.ico" />
-      </Head>
-      <h1>Lets build an IPFS dashboard!</h1>
+       <div className="center">
+      <p>The text within the div is centered horizontally.</p>
+      </div>
     </div>
   );
 }
