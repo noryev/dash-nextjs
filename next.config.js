15f0f1a8473fc3e5b09c518abb362a3474c44d26
Logan Lentz <logan.lentz1@gmail.com>
2022-09-05 22:45:19 -0500

    text color still out of spec

--- next.config.js
+++ next.config.js
@@ -1,7 +1,2 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
-
-module.exports = nextConfig
+const withImages = require("next-images");
+module.exports = withImages({});
--- structure.md
+++
@@ -1,7 +0,0 @@
-In this project
-
-    -- files needed
-    1. laftnavbar.js
-    2. harder.js
-    3. Content.js
-
