d4686e3cbbed6cf7650e5b5b265e6da08ff58011
Logan Lentz <30084404+noryev@users.noreply.github.com>
2022-09-11 00:36:14 -0500

    ipfs version UI post

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
 import '../styles/globals.css'
 import React from "react";
+import { useState, useEffect } from React,
+import { create } from 'ipfs-core'
 
+const IpfsComponent = () => {
+  const [id, setId] = useState(null);
+  const [ipfs,setIpfs] = useState(null);
+  const [version, setVersion] = useState(false);
 
+  useEffect(() => {
+    const init = async () => {
+      if (ipfs) return 
+      const node = await create(); 
+      const nodeId = await node.id() 
+    }
+    
+  })
+
+}
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
