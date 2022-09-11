a5efad7826cec299aa0fb8292d3dacebf7c7aa88
Logan Lentz <30084404+noryev@users.noreply.github.com>
2022-09-11 04:17:37 -0500

    borked a few things

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,5 +1,42 @@
 import React from "react";
 import styles from "../../styles/Home.module.css";
+import { useState, useEffect } from 'react'
+import { create } from 'ipfs-core'
+
+
+
+
+const IpfsComponent = () => {
+  const [id, setId] = useState(null);
+  const [ipfs,setIpfs] = useState(null);
+  const [version, setVersion] = useState(null);
+  const [isOnline, setIsOnline] = useState(false);
+
+  useEffect(() => {
+    const init = async () => {
+      if (ipfs) return 
+      const node = await create(); 
+
+      const nodeId = await node.id();
+      const nodeVersion = await node.version();
+      const nodeIsOnline = node.isOnline();
+
+      setIpfs(node);
+      setId(nodeId.id);
+      setVersion(nodeVersion.version);
+      setIsOnline(nodeIsOnline);
+
+    }
+    
+    init()
+  }, [ipfs]);
+
+  if (!ipfs || !id) {
+    return <h4>Connecting to IPFS....</h4>
+  }
+  
+// now make it show up :D
+
 
 function Header() {
 	return (
@@ -11,13 +48,16 @@ function Header() {
 					</h2>
 					<p>welcome to the board</p>
 				</div>
-
+				<div> 
+     		<h4 data-test="id">ID: {id.toString()}</h4>
+    		<h4 data-test="version">Version: {version}</h4>
+     		<h4 data-test="status">Status: {isOnline ? 'Online' : 'Offline' }</h4>
+    	</div>
+	
+  			</div>
 				</div>
-			</div>
-		
+			
 	);
 }
-
-// holly fk git 
-
+}
 export default Header;
